f86660f4518e6420a68d044cb769059a42e88a5d
Laakso Jani <jani.laakso@fi.fujitsu.com>
2020-07-20 17:27:37 +0300

    update test file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('simona app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('simona2 app is running!');
   });
 });
