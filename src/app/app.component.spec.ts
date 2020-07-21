c0bb2d3aac9d220070a126a4c0b8dd0df852f576
Laakso Jani <jani.laakso@fi.fujitsu.com>
2020-07-21 12:11:57 +0300

    update text

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'simona2'`, () => {
+  it(`should have as title 'simona3'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('simona2');
+    expect(app.title).toEqual('simona3');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('simona2 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('simona3 app is running!');
   });
 });
